dcba7f6076ab60e0e28108e703580c5a13c6ecd4
Daeki <kimdaeki@gmail.com>
2021-08-09 11:49:56 +0900

    1

--- src/com/iu/s1/Study9Main.java
+++ src/com/iu/s1/Study9Main.java
@@ -1,9 +1,17 @@
 package com.iu.s1;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.iu.s1.location.LocationDAO;
+
 public class Study9Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LocationDAO locationDAO = new LocationDAO();
+		locationDAO.getList();
+
+		
 
 	}
 
---
+++ src/com/iu/s1/location/LocationDAO.java
@@ -0,0 +1,76 @@
+package com.iu.s1.location;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.iu.s1.util.DBConnect;
+
+public class LocationDAO {
+	
+	private DBConnect dbConnect;
+	
+	public LocationDAO() {
+		dbConnect = new DBConnect();
+	}
+	
+	public void getOne(int location_id) {
+		
+		
+		
+		
+	}
+	
+	
+	public void getList() {
+		//LOCATIONS Table을 조회해서 출력
+		
+	
+		
+		//2. Driver를 메모리에 로딩
+		Connection con = null;
+		PreparedStatement st=null;
+		ResultSet rs=null;
+		try {
+			
+		
+		//3. DB Connection
+			con = dbConnect.getConnect();
+			System.out.println("DB 연결성공");
+			
+		//4. SQL문 생성
+			String sql ="SELECT * FROM Locations";
+		
+		//5. 미리 전송
+			st = con.prepareStatement(sql);
+			
+		//6. 최종 전송 후 결과 처리
+			rs = st.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println(rs.getString("STREET_ADDRESS"));
+				System.out.println(rs.getInt("LOCATION_ID"));
+				System.out.println("------------");
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				st.close();
+				rs.close();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+	}
+
+}
